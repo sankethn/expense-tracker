50b90a7edee901f717f565a83da35f4e749e2c13
sankethn <69240053+sankethn@users.noreply.github.com>
2021-11-10 17:45:38 +0530

    Add files via upload

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterExpenseHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onFilterYear={filterExpenseHandler}
+      />
+      <ExpensesChart expenses={filteredExpenses} />
+      <ExpensesList items={filteredExpenses} />
+    </Card>
+  );
+}
+
+export default Expenses;
---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
+
+function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+
+    props.onAddExpense(expenseData);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing((prevState) => !prevState);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          toggleForm={startEditingHandler}
+        />
+      )}
+    </div>
+  );
+}
+
+export default NewExpense;
